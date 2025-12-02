29ac472eb6df1643fdad1a45e215cf8f44b03600
Michael Limiero <mike5713@gmail.com>
2025-12-02 00:23:18 -0500

    day 2

---
+++ day02.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+def parse(input)
+  input.split(",").map(&:strip).map { |x| x.split("-").map { |x| x.to_i } }
+end
+
+def invalid?(num)
+  s = num.to_s
+  i = s.length / 2
+  s[0...i] == s[i..]
+end
+
+def part_one(input)
+  ranges = parse(input)
+  res = 0
+  ranges.each do |range|
+    r = range[0]..range[1]
+    r.filter { |x| invalid?(x) }.each do |x|
+      res += x
+    end
+  end
+  res
+end
+
+def invalid_two?(num)
+  s = num.to_s
+  (2..s.length).each do |n|
+    if s.length % n == 0
+      return true if s == s[0...(s.length/n)] * n
+    end
+  end
+  false
+end
+
+def part_two(input)
+  ranges = parse(input)
+  res = 0
+  ranges.each do |range|
+    r = range[0]..range[1]
+    r.filter { |x| invalid_two?(x) }.each do |x|
+      res += p x
+    end
+  end
+  res
+end
+
+input = ARGF.read
+puts part_one(input)
+puts part_two(input)
